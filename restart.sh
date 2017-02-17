6a4d64128e071451b03beb571b64722c92cd5c45
闫广坤 <yanguangkun@126.com>
2017-02-17 14:40:29 +0800

    注掉注释

--- restart.sh
+++ restart.sh
@@ -18,13 +18,13 @@ $CATALINA_HOME/bin/shutdown.sh>/dev/null 2>&1
 #
 for pid in $(ps -ef|grep $CATALINA_HOME|awk '{print $2,$8}'|grep java|awk '{print $1}'); do
     echo "停止运行 $CATALINA_HOME 进程id:$pid"
-    #kill -9 $pid
+    kill -9 $pid
 done
 
 # 启动tomcat
 if [ "$1" != "stop" ]; then
   echo "重启tomcat"
   echo "tomcat 目录 $CATALINA_HOME"
-  #$CATALINA_HOME/bin/startup.sh
+  $CATALINA_HOME/bin/startup.sh
   echo "重启完成"
 fi
