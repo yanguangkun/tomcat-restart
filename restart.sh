61624db873463aeacb341dedbe8404b1338a82c9
闫广坤 <yanguangkun@126.com>
2017-02-17 14:33:25 +0800

    添加shutdown.sh结束tomcat

--- restart.sh
+++ restart.sh
@@ -9,16 +9,22 @@ PRGDIR=`dirname "$PRG"`
 # 获取tomcat目录
 CATALINA_HOME=`cd "$PRGDIR/.." >/dev/null; pwd`
 
-# 获取tomcat进程并杀死进程
+# 停止运行tomcat
+$CATALINA_HOME/bin/shutdown.sh>/dev/null 2>&1
+
+#
+# 如果 shutdown.sh 结束不了，在通过kill结束tomcat
+# 获取tomcat进程并结束进程
+#
 for pid in $(ps -ef|grep $CATALINA_HOME|awk '{print $2,$8}'|grep java|awk '{print $1}'); do
     echo "停止运行 $CATALINA_HOME 进程id:$pid"
-    kill -9 $pid
+    #kill -9 $pid
 done
 
 # 启动tomcat
 if [ "$1" != "stop" ]; then
   echo "重启tomcat"
   echo "tomcat 目录 $CATALINA_HOME"
-  $CATALINA_HOME/bin/startup.sh
+  #$CATALINA_HOME/bin/startup.sh
   echo "重启完成"
 fi
